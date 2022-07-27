91c7ea34210276435620f7f65b8f658ada7142ef
Udara <uartcreations@gmail.com>
2022-07-27 14:39:09 +0530

    My First Change

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          {/* My Git Bash Change added*/}
+          Add My First Change
         </p>
         <a
           className="App-link"
